03a0dfc69080bc51461300e6ef3711d1f6c1f717
warhodes-dev <warhodes@gmail.com>
2023-11-15 14:15:34 -0500

    testing authentication methods

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,50 @@ use anyhow::Result;
 use atrium_api::{client::AtpServiceClient, agent::AtpAgent};
 use atrium_xrpc_client::reqwest::ReqwestClient;
 
+use serde::ser::Serialize;
+
 use atrium_api::com::atproto::server::create_session::Input;
+use atrium_api::agent::Session;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let api_id = "kotblini.bsky.social";
-    let api_key = std::fs::read_to_string("./api.key")?;
+    let api_id = std::fs::read_to_string("./api.id")?.trim().to_owned();
+    let api_key = std::fs::read_to_string("./api.key")?.trim().to_owned();
 
-    let agent = Arc::new(AtpAgent::new(ReqwestClient::new("https://bsky.social")));
-    agent.login(&api_id, &api_key).await?;
+    let agent = authenticate(api_id, api_key).await?;
 
-    let actors = ["safety.bsky.app", "bsky.app"];
-    let handles = actors
-        .iter()
-        .map(|&actor| {
-            let agent = Arc::clone(&agent);
-            tokio::spawn(async move {
-                agent.api.app.bsky.actor
-                    .get_profile(atrium_api::app::bsky::actor::get_profile::Parameters {
-                        actor: actor.into(),
-                    })
-                    .await
-            })
-        })
-        .collect::<Vec<_>>();
-
-    let results = join_all(handles).await;
-
-    for (actor, result) in actors.iter().zip(results) {
-        println!("{actor}: {:#?}", result??);
-    }
+    println!("{:#?}", agent.get_session().await);
 
     Ok(())
+}
+
+async fn authenticate(
+    id: String,
+    key: String,
+) -> Result<AtpAgent<ReqwestClient>> {
+
+    let agent = AtpAgent::new(ReqwestClient::new("https://bsky.social"));
+    
+    if let Ok(old_session_json) = std::fs::read_to_string("./session.json") {
+        println!("Logging in with cached session");
+        let old_session: Session = serde_json::from_str(&old_session_json)?;
+        match agent.resume_session(old_session).await {
+            Ok(_) => {
+                eprintln!("Cached session successfully validated");
+                return Ok(agent)
+            },
+            Err(e) => {
+                eprintln!("Error resuming session: {e}");
+                eprintln!("Trying reauthentication");
+            }
+        }
+    }
+
+    println!("Logging in as {id} ({key})");
+    let session = agent.login(&id, &key).await?;
+    let session_json = serde_json::to_string(&session)?;
+    println!("App-password login successful. Caching session to ./session.json");
+    std::fs::write("./session.json", session_json)?;
+
+    Ok(agent)
 }
