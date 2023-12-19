f852f68485af2d18ac56e87fab4c5e4f8a73f5da
warhodes-dev <warhodes@gmail.com>
2023-12-19 13:44:39 -0500

    prototype feed

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::{sync::Arc, path::Path, fs};
 use futures::future::join_all;
 
 use anyhow::Result;
-use atrium_api::{client::AtpServiceClient, agent::AtpAgent};
+use anyhow::anyhow;
+use atrium_api::agent::{AtpAgent, store::MemorySessionStore};
 use atrium_xrpc_client::reqwest::ReqwestClient;
 
+use promptly::prompt;
 use serde::{Serialize, Deserialize};
 
 use atrium_api::com::atproto::server::create_session::Input;
@@ -20,63 +22,129 @@ async fn main() -> Result<()> {
     let api_id = std::fs::read_to_string("./api.id")?.trim().to_owned();
     let api_key = std::fs::read_to_string("./api.key")?.trim().to_owned();
 
-    let agent = authenticate(api_id, api_key).await?;
+    let mut agent = AtpAgent::new(
+        ReqwestClient::new("https://bsky.social"),
+        MemorySessionStore::default(),
+    );
 
-    Ok(())
-}
-
-async fn authenticate(
-    id: String,
-    key: String,
-) -> Result<AtpAgent<ReqwestClient>> {
-
-    let agent = AtpAgent::new(ReqwestClient::new("https://bsky.social"));
-    
     let cookie_path = dirs::home_dir().unwrap().as_path().join(".local/bsky_auth");
+    if try_restore_session(&mut agent, &cookie_path).await.is_err() {
+        let cookie = authenticate(&mut agent, api_id, api_key).await?;
+        cache_session(cookie, &cookie_path)?
+    }
 
-    if let Ok(baked_cookie) = std::fs::read_to_string(&cookie_path) {
+    eprintln!("Login successful");
 
-        let session_cookie = SessionCookie::from_str(&baked_cookie)?;
-
-        let old_session = Session {
-            access_jwt: session_cookie.access,
-            did: session_cookie.did,
-            did_doc: None,
-            email: None,
-            email_confirmed: None,
-            handle: String::new(),
-            refresh_jwt: session_cookie.refresh,
+    let mut cursor = None;
+    loop {
+        println!("Feching batch of 10");
+        let get_timeline_params = atrium_api::app::bsky::feed::get_timeline::Parameters {
+            algorithm: None,
+            cursor: None,
+            limit: Some(10),
         };
+        let timeline = agent.api.app.bsky.feed.get_timeline(get_timeline_params).await?;
 
-        println!("Logging in with cached session");
-        println!("Old session: {old_session:#?}");
+        for post in timeline.feed.iter().map(|feed_item| &feed_item.post) {
+            cursor = Some(post.cid.clone());
+            match &post.record {
+                atrium_api::records::Record::AppBskyActorProfile(_) => todo!(),
+                atrium_api::records::Record::AppBskyFeedGenerator(_) => todo!(),
+                atrium_api::records::Record::AppBskyFeedLike(_) => todo!(),
+                atrium_api::records::Record::AppBskyFeedPost(record) => print_post(&post, &record),
+                atrium_api::records::Record::AppBskyFeedRepost(_) => todo!(),
+                atrium_api::records::Record::AppBskyFeedThreadgate(_) => todo!(),
+                atrium_api::records::Record::AppBskyGraphBlock(_) => todo!(),
+                atrium_api::records::Record::AppBskyGraphFollow(_) => todo!(),
+                atrium_api::records::Record::AppBskyGraphList(_) => todo!(),
+                atrium_api::records::Record::AppBskyGraphListblock(_) => todo!(),
+                atrium_api::records::Record::AppBskyGraphListitem(_) => todo!(),
+            }
 
-        match agent.resume_session(old_session).await {
-            Ok(_) => {
-                eprintln!("Cached session successfully validated");
-                let sesh = agent.get_session().await.unwrap();
-                println!("Validated session: {sesh:#?}");
-                return Ok(agent)
-            },
-            Err(e) => {
-                eprintln!("Error resuming session: {e}");
-                eprintln!("Trying reauthentication");
+            let mut buf = String::new();
+            std::io::stdin().read_line(&mut buf)?;
+
+            match buf.as_str() {
+                "debug" => println!("{post:#?}"),
+                "cursor" => println!("{cursor:?}"),
+                _ => (),
             }
         }
     }
 
-    println!("Logging in as {id} ({key})");
+    Ok(())
+}
+
+fn print_post(
+    post: &atrium_api::app::bsky::feed::defs::PostView, 
+    record: &atrium_api::app::bsky::feed::post::Record) 
+{
+    if let Some(display_name) = &post.author.display_name {
+        println!("{}", display_name)
+    }
+    println!("@{}\n", post.author.handle);
+    println!("{}", record.text);
+    println!("Replies: {}\tLikes: {}\tReposts: {}",
+        post.like_count.unwrap_or(0),
+        post.reply_count.unwrap_or(0),
+        post.repost_count.unwrap_or(0),
+    )
+}
+
+fn get_command() {
+    let cmd: String = prompt("> ").unwrap();
+
+}
+
+async fn try_restore_session(
+    agent: &mut AtpAgent<MemorySessionStore, ReqwestClient>, 
+    session_cookie_path: &Path
+) -> Result<()> {
+    
+    let baked_cookie = std::fs::read_to_string(&session_cookie_path)?;
+    let session_cookie = SessionCookie::from_str(&baked_cookie)?;
+
+    let old_session = Session {
+        access_jwt: session_cookie.access,
+        did: session_cookie.did,
+        did_doc: None,
+        email: None,
+        email_confirmed: None,
+        handle: String::new(),
+        refresh_jwt: session_cookie.refresh,
+    };
+
+    eprintln!("Logging in with cached session");
+    eprintln!("Old session: {old_session:#?}");
+
+    agent.resume_session(old_session).await?;
+
+    eprintln!("Cached session successfully validated");
+    return Ok(())
+}
+
+async fn authenticate(
+    agent: &mut AtpAgent<MemorySessionStore, ReqwestClient>,
+    id: String,
+    key: String,
+) -> Result<SessionCookie> {
+    eprintln!("Logging in as {id} ({key})");
     let session = agent.login(&id, &key).await?;
-    println!("App-password login successful.");
+    eprintln!("App-password login successful.");
 
-    print!("Caching session");
-    let cookie = SessionCookie::from_session(&session).to_string();
-    println!(" to {}", &cookie_path.display());
-    fs::create_dir_all(cookie_path.parent().unwrap())?;
-    fs::File::create(&cookie_path)?;
-    fs::write(&cookie_path, cookie)?;
+    let cookie = SessionCookie::from_session(&session);
+    Ok(cookie)
+}
 
-    Ok(agent)
+fn cache_session(cookie: SessionCookie, path: &Path) -> Result<()> {
+    fs::create_dir_all(path.parent().unwrap())?;
+    fs::File::create(&path)?;
+    fs::write(&path, cookie.to_string())?;
+    Ok(())
+}
+
+struct BlackskyAuth {
+
 }
 
 #[derive(Serialize, Deserialize)]
